dc3269c0aac30d111bdea1d6bac645eaf110e6b7
SHANG-TING, SIE <rebom8888@gmail.com>
2018-01-13 10:28:37 +0800

    Kevin大大 一開始的一些基本熱身 https://paper.dropbox.com/doc/Reactive-Form-Pvoxuc90VS0VvMH3nFdDb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  get phones(): FormArray { return this.form.get('phones') as FormArray; }
+
+  form = this._fb.group({
+    firstName: [{ value: '123', disabled: true }],
+    lastName: [undefined, [Validators.required]],
+    phones: this._fb.array([
+      this.getPhoneControl()
+    ])
+  });
+
+  constructor(private _fb: FormBuilder) { }
+
+  private getPhoneControl() {
+    return this._fb.control(undefined, [
+      Validators.required,
+      Validators.pattern(/^[09]{2}[0-9]{8}$/)
+    ]);
+  }
+
+  addPhone(): void {
+    this.phones.push(this.getPhoneControl());
+  }
+
+  removePhone(index: number): void {
+    this.phones.removeAt(index);
+  }
+
+  submit(): void {
+
+  }
+
 }
